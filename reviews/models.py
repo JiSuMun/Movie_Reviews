8559828eba0157002cd9034a8345a08957c539d1
zzgh06 <zzgh06@naver.com>
2023-04-14 10:27:16 +0900

    topic-g

--- reviews/models.py
+++ reviews/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
+
 # Create your models here.
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=10)
     content = models.TextField()
     movie = models.CharField(max_length=20)
-    image = models.ImageField(null=True, blank=True)
+    image = ProcessedImageField(blank=True, upload_to='images/',
+        processors=[ResizeToFit(60, 100)],
+        format='JPEG',
+        options={'quality': 90}) 
     
 class Comment(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
